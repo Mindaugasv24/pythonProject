7d9ebe9fab13450121f2ad27744c28d5535db70c
Mindaugasv24 <165074498+Mindaugasv24@users.noreply.github.com>
2024-10-10 12:06:54 +0300

    update

--- sample.py
+++ sample.py
@@ -1,3 +1,7 @@
 player_img = pygame.image.load('C:/Users/k00zeris/Documents/pythonProject/5games-main/space shooter/images/player.png')
 enemy_img = pygame.image.load('C:/Users/k00zeris/Documents/pythonProject/5games-main/space shooter/images/meteor.png')
 bullet_img = pygame.image.load('C:/Users/k00zeris/Documents/pythonProject/5games-main/space shooter/images/laser.png')
+
+pygame.mixer.music.load('C:/Users/k00zeris/Documents/pythonProject/5games-main/space shooter/audio/game_music.wav')
+
+shoot_sound = pygame.mixer.Sound('C:/Users/k00zeris/Documents/pythonProject/5games-main/space shooter/audio/laser.wav')
